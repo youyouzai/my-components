978eacb3d9162bd750663c3cea3bccaeedfba64b
liuzengguang <823375476@qq.com>
2019-07-20 12:23:32 +0800

    feature: 完成表格基本功能

--- README.md
+++ README.md
@@ -1,29 +1,13 @@
 # my-components
 
-## Project setup
+## 启动json-server
 ```
-npm install
+npm install -g json-server
+
+json-server --watch db.json
 ```
 
-### Compiles and hot-reloads for development
+### 启动工程
 ```
 npm run serve
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/simple-table.vue
+++ src/components/simple-table.vue
@@ -2,38 +2,58 @@
   <div>
        <el-table
         ref="multipleTable"
-        :data="options.data"
+        :data="tableData"
+        :height="height"
+        :border="border"
+        :stripe="stripe"
+        :show-header="showHeader"
         style="width: 100%">
-        <el-table-column v-if="hasCheckBox"  type="selection" width="70"></el-table-column>
-        <el-table-column  
-            v-for="(column, index) in columns"
-            :key="index"
-            :type="column.type"
-            :label="column.label"
-            :prop="column.prop"
-            :width="column.width">
-            <template  slot-scope="scope">
-                <simple-table-render :options="column" :row="scope.row"></simple-table-render>
+            <template v-if="options">
+                <el-table-column v-if="hasCheckBox"  type="selection" width="70"></el-table-column>
+                <el-table-column  
+                    v-for="(column, index) in columns"
+                    :key="index"
+                    :type="column.type"
+                    :label="column.label"
+                    :prop="column.prop"
+                    :width="column.width">
+                    <template  slot-scope="scope">
+                        <simple-table-render :options="column" :row="scope.row"></simple-table-render>
+                    </template>
+                </el-table-column>
             </template>
-        </el-table-column>
+            <slot></slot>
        </el-table>
+       <div class="pagination-cotnainer" >
+            <el-pagination
+                :current-page="form.pageNo"
+                :page-sizes="[10, 20, 50, 100, 200]"
+                :page-size="form.pageSize"
+                :total="totalCount"
+                layout="total, sizes, prev, pager, next, jumper"
+                @size-change="changeSize"
+                @current-change="changePage"></el-pagination>
+        </div>
   </div>
 </template>
 
 <script>
 import './table-render'
+import {httpGet} from  '@/util/http'
 export default {
-    props:{
-        options: {
-            type: Object
-        }
-    },
+    props:['options', 'data', 'height', 'stripe', 'border', 'size', 'showHeader'],
     data(){
         return {
-            
+            url: '',
+            form: {
+                pageSize: 10,
+                pageNo: 1,
+            },
+            totalCount: 0,
         }
     },
     computed: {
+        
         columns(){
             let columns = this.options.columns
             if(!columns) return []
@@ -47,18 +67,50 @@ export default {
             return columns && columns.some(column => {
                 return column.type === 'selection'
             })
+        },
+        tableData(){
+            return this.data || this.options.data
+        }
+    },
+    mounted() {
+        if(this.options) {
+            this.url = this.options.url;
+            this.query();
         }
     },
     methods: {
-        nameRender(h, row){
-            return h('p', 'hello, ' + row.name)
+        async query(params) {
+            if(!this.url) return;
+            this.form = {
+                ...this.form,
+                ...params
+            }
+            const request = this.httpGet_ || httpGet;
+            const response = await request(this.url, this.form);
+            this.data = response.data;
+            const totalField = this.options.totalField || 'totalCount'
+            this.totalCount = response[totalField];
         },
-        imgRender(row){
-            return '这是一个链接：' + row.imgUrl
-        }
+        reset(){
+           this.pageSize = 10
+           this.pageNo = 1
+           this.query()
+        },
+        changePage (val) {
+            this.form.pageNo = val;
+            this.query();
+        },
+        changeSize (val) {
+            this.option.pageSize = val;
+            this.query();
+        },
     }
 }
 
 </script>
-<style>
+<style scoped>
+    .pagination-cotnainer{
+        margin-top: 20px;
+        text-align: right;
+    }
 </style>
